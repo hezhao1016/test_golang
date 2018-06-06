9112371834f08d8fc846036d4cbc46a50f857d8c
hezhao <1439293823@qq.com>
2018-06-06 18:36:15 +0800

    数据类型

--- src/logic/basic.go
+++ src/logic/basic.go
@@ -14,6 +14,32 @@ func main()  {
 	// 如果你打算将多个语句写在同一行，它们则必须使用 ; 人为区分，但在实际开发中我们并不鼓励这种做法。
 	fmt.Println("Hello");fmt.Println("Golang!")
 
+	// 单行注释
+	/* 我是多行注释 */
 
-	
+	// import 导包
+	/*
+	import "fmt"最常用的一种形式
+
+	import "./test"导入同一目录下test包中的内容
+
+	import f "fmt"导入fmt，并给他启别名f
+
+	import . "fmt"，将fmt启用别名"."，这样就可以直接使用其内容，而不用再添加ｆｍｔ，如fmt.Println可以直接写成Println
+
+	import  _ "fmt" 表示不使用该包，而是只是使用该包的init函数，并不显示的使用该包的其他内容。注意：这种形式的import，当import时就执行了fmt包中的init函数，而不能够使用该包的其他函数。
+	*/
+
+	// const 关键字来进行常量的定义。
+
+	// 在函数体外部使用 var 关键字来进行全局变量的声明和赋值。
+
+	// type 关键字来进行结构(struct)和接口(interface)的声明。
+
+	// func 关键字来进行函数的声明。
+
+	// 可见性规则
+	// Go语言中，使用大小写来决定该常量、变量、类型、接口、结构或函数是否可以被外部包所调用。
+	// 函数名首字母小写即为 private
+	// 函数名首字母大写即为 public
 }
---
+++ src/logic/data_type.go
@@ -0,0 +1,84 @@
+package main
+
+// 数据类型
+
+/*
+Go 语言按类别有以下几种数据类型：
+布尔型
+	bool， 值是 true 或 false，默认为 false
+数字类型
+	整形
+		uint8	无符号 8 位整型 (0 到 255)
+		uint16	无符号 16 位整型 (0 到 65535)
+		uint32	无符号 32 位整型 (0 到 4294967295)
+		uint64	无符号 64 位整型 (0 到 18446744073709551615)
+		int8	有符号 8 位整型 (-128 到 127)
+		int16	有符号 16 位整型 (-32768 到 32767)
+		int32	有符号 32 位整型 (-2147483648 到 2147483647)
+		int64	有符号 64 位整型 (-9223372036854775808 到 9223372036854775807)
+	浮点数
+		float32		IEEE-754 32位浮点型数
+		float64		IEEE-754 64位浮点型数
+		complex64	32 位实数和虚数
+		complex128	64 位实数和虚数、
+	其他数字类型
+		byte	类似 uint8
+		rune	类似 int32
+		uint	32 或 64 位
+		int		与 uint 一样大小
+		uintptr	无符号整型，用于存放一个指针
+字符串
+	string
+派生类型:
+	包括：
+	(a) 指针类型（Pointer）
+	(b) 数组类型
+	(c) 结构化类型(struct)
+	(d) Channel 类型
+	(e) 函数类型
+	(f) 切片类型
+	(g) 接口类型（interface）
+	(h) Map 类型
+*/
+
+import (
+	"fmt"
+	"strings"
+)
+
+var isActive bool  // 全局变量声明
+var enabled, disabled = true, false  // 忽略类型的声明
+
+func main() {
+	var available bool  // 一般声明
+	valid := false      // 简短声明
+	available = true    // 赋值操作
+
+	fmt.Println(isActive)
+	fmt.Println(enabled)
+	fmt.Println(disabled)
+	fmt.Println(available)
+	fmt.Println(valid)
+
+	// 数字
+	var a = 2
+	var b = 1.5
+	var c float32 = 1.5  // 指定变量类型为float
+
+	fmt.Println("------------------------------------------")
+	fmt.Print(a)  // 输出不换行
+	fmt.Println()
+	fmt.Println(b)
+	fmt.Println(c)
+
+	// 字符串去除空格和换行符
+	str := "这里是 www\n.runoob\n.com"
+	fmt.Println("-------- 原字符串 ----------")
+	fmt.Println(str)
+	// 去除空格
+	str = strings.Replace(str, " ", "", -1)
+	// 去除换行符
+	str = strings.Replace(str, "\n", "", -1)
+	fmt.Println("-------- 去除空格与换行后 ----------")
+	fmt.Println(str)
+}
