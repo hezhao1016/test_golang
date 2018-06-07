6a06bec004d6eb8363bdaece79bdd1c8b62ed837
hezhao <1439293823@qq.com>
2018-06-07 23:37:37 +0800

    func

--- src/logic/func.go
+++ src/logic/func.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 /*
 Go 语言函数
@@ -64,17 +67,68 @@ func maxNumber(first int, others ...int) int {
 
 
 // ----------> 函数参数
+// 默认情况下，Go 语言使用的是值传递，即在调用过程中不会影响到实际参数。
 // 值传递
+func swap(x, y int) {
+	// 交换值
+	x, y = y, x
+}
 
-// 引用传递
+// 引用传递, *int 指针，指向整型
+func swapRef(x *int, y *int) {
+	// 交换值
+	x, y = y, x
+}
 
 // ----------> 函数用法
-
 // 函数作为值
+/* 声明函数变量, 匿名函数 */
+var getSquareRoot = func(x float64) float64 {
+	return math.Sqrt(x)
+}
 
 // 闭包
+// Go 语言支持匿名函数，可作为闭包。匿名函数是一个"内联"语句或表达式。匿名函数的优越性在于可以直接使用函数内的变量，不必申明。
+func getSequence() func() int {
+	i:=0
+	return func() int {
+		i+=1
+		return i
+	}
+}
+//带参数的闭包函数
+func add(x1, x2 int) func()(int, int)  {
+	i := 0
+	return func() (int, int){
+		i++
+		return i, x1+x2
+	}
+}
+
+// 多层次参数
+func add2(x1, x2 int) func(x3 int, x4 int)(int, int, int)  {
+	i := 0
+	return func(x3 int, x4 int) (int, int, int){
+		i++
+		return i, x1+x2, x3+x4
+	}
+}
+
 
 // 方法
+// Go 语言中同时有函数和方法。一个方法就是一个包含了接受者的函数，接受者可以是命名类型或者结构体类型的一个值或者是一个指针。所有给定类型的方法属于该类型的方法集。
+
+/* 定义结构体 */
+type Circle struct {
+	radius float64
+}
+
+//该 method 属于 Circle 类型对象中的方法
+func (c Circle) getArea() float64 {
+	//c.radius 即为 Circle 类型对象中的属性
+	return 3.14 * c.radius * c.radius
+}
+
 
 
 
@@ -106,7 +160,6 @@ func showYangHuiTsriangle() {
 }
 
 
-
 func main() {
 	// 无参
 	hello()
@@ -122,6 +175,58 @@ func main() {
 
 	// 可变参数
 	fmt.Println(maxNumber(1, 9, 6, 12, 34, 5))
+
+	// 值传递
+	a := 100
+	b := 200
+	fmt.Printf("值传递交换前 a, b 的值为 : %d, %d\n", a, b )
+	swap(a, b)
+	fmt.Printf("值传递交换后 a, b 的值为 : %d, %d\n", a, b )
+
+	// 引用传递
+	fmt.Printf("引用传递交换前 a, b 的值为 : %d, %d\n", a, b )
+	// &a 指向 a 指针，a 变量的地址
+	swapRef(&a, &b)
+	fmt.Printf("引用传递交换后 a, b 的值为 : %d, %d\n", a, b )
+
+	// 函数作为值
+	fmt.Println(getSquareRoot(9))
+
+	// 闭包
+	fmt.Println("------------------闭包---------------------")
+	/* nextNumber 为一个函数，函数 i 为 0 */
+	nextNumber := getSequence()
+
+	/* 调用 nextNumber 函数，i 变量自增 1 并返回 */
+	fmt.Println(nextNumber())
+	fmt.Println(nextNumber())
+	fmt.Println(nextNumber())
+
+	/* 创建新的函数 nextNumber1，并查看结果 */
+	nextNumber1 := getSequence()
+	fmt.Println(nextNumber1())
+	fmt.Println(nextNumber1())
+
+	// 带参数的闭包函数调用
+	fmt.Println("---------------------------------------")
+	add_func := add(1,2)
+	fmt.Println(add_func())
+	fmt.Println(add_func())
+	fmt.Println(add_func())
+
+	fmt.Println("---------------------------------------")
+	add_func2 := add2(1,2)
+	fmt.Println(add_func2(1, 1))
+	fmt.Println(add_func2(0, 0))
+	fmt.Println(add_func2(2, 2))
+
+
+	// 使用方法
+	fmt.Println("-------------------方法--------------------")
+	var c1 Circle
+	c1.radius = 10.00
+	fmt.Println("Area of Circle(c1) = ", c1.getArea())
+
 
 	// 杨辉三角
 	fmt.Println("--------- 杨辉三角 ---------")
--- src/logic/operator.go
+++ src/logic/operator.go
@@ -112,7 +112,7 @@ func mathOperator() {
 	fmt.Printf("第五行 - c 的值为 %d\n", c )
 	a++
 	fmt.Printf("第六行 - a 的值为 %d\n", a )
-	a=21   // 为了方便测试，a 这里重新赋值为 21
+	a = 21   // 为了方便测试，a 这里重新赋值为 21
 	a--
 	fmt.Printf("第七行 - a 的值为 %d\n", a )
 }
